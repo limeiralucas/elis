f469343a51323ea1f1dbdbad50deb98bc1b01314
Lucas Limeira <lucasalveslm@gmail.com>
2013-11-12 22:59:16 -0300

    criação e gravação de arquivo

--- main.cpp
+++ main.cpp
@@ -3,31 +3,46 @@
 #include <list>
 #include <string>
 #include <cstdlib>
+#include <conio.h>
 
 using namespace std;
 
+void save(list<string> &l, fstream &f){
+	f.clear();
+
+	for (list<string>::iterator it=l.begin(); it != l.end(); ++it){
+			f << *it << "\n";
+	}
+}
+
 int main(int argc, char** argv) {
 	bool exit = false;
-	
+
 	fstream file;
 	string line;
 	string tmp;
-	file.open("file.txt");
+	file.open("file.txt", ios_base::in | ios_base::out | ios_base::app);
 	list<string> scr;
 	while(getline(file, line)){
 			scr.insert(scr.end(), line);
 	}
-	
+
 	if(file.is_open())
 	{
 		while(!exit){
+            file.clear();
 			system("clear");
 			for (std::list<string>::iterator it=scr.begin(); it != scr.end(); ++it){
 				cout << *it << endl;
 			}
 			getline(std::cin, tmp);
+			if(tmp == "eof")
+                break;
 			scr.insert(scr.end(), tmp);
 		}
+
+		save(scr, file);
+		file.close();
 	}
 	return 0;
 }
