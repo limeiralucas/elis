2b124b83aaef02f5d87e538bfaed1b4029db746d
Lucas Limeira <lucasalveslm@gmail.com>
2013-11-15 16:06:13 -0300

    Detecção de comandos e carregamento/escrita em arquivo

--- main.cpp
+++ main.cpp
@@ -1,48 +1,107 @@
 #include <iostream>
-#include <fstream>
-#include <list>
+#include <conio.h>
 #include <string>
 #include <cstdlib>
+#include <cstdio>
+#include <windows.h>
+#include <list>
+#include <fstream>
 
 using namespace std;
 
-void save(list<string> &l, fstream &f){
-	f.clear();
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int getConsoleLine(){
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if(GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
+        return csbi.dwCursorPosition.Y + 1;
+}
 
-	for (list<string>::iterator it=l.begin(); it != l.end(); ++it){
-			f << *it << "\n";
+void flip_screen(list<string> &buffer){
+	system("cls");
+	for(list<string>::iterator i = buffer.begin(); i != buffer.end(); i++)
+		cout << getConsoleLine() << " " << *i << "\n";
+	cout << "--------------------------------\n";
+}
+
+void file_load(ifstream &file, list<string> &buffer){
+	string line;
+	while(getline(file, line))
+		buffer.push_back(line);
+}
+
+void save(list<string> &buffer, ofstream &destination){
+	for(list<string>::iterator i = buffer.begin(); i != buffer.end(); i++)
+		destination << *i << "\n";
+}
+
+void menu(bool &detect, bool &exit, list<string> &buffer, ofstream &destination, bool &overwrite){
+	char key;
+	cout << "\nW - Write\tQ - Quit";
+	cout << "\n:";
+	key = getch();
+	switch(key){
+		case 'w':
+		case 'W':
+			save(buffer, destination);
+		break;
+		case 'q':
+		case 'Q':
+			detect = false;
+			exit = true;
+			overwrite = true;
+		break;
+		default:
+		break;
 	}
 }
 
 int main(int argc, char** argv) {
+	char key;
 	bool exit = false;
-
-	fstream file;
-	string line;
-	string tmp;
-	file.open("file.txt", ios_base::in | ios_base::out | ios_base::app);
-	list<string> scr;
-	while(getline(file, line)){
-			scr.insert(scr.end(), line);
-	}
-
-	if(file.is_open())
-	{
-		while(!exit){
-            int line_count = 1;
-            file.clear();
-			system("clear");
-			for (std::list<string>::iterator it=scr.begin(); it != scr.end(); ++it){
-				cout << line_count++ << " " << *it << endl;
+	bool detect = false;
+	bool overwrite = false;
+	string line_buffer = "";
+	list<string> buffer;
+	
+	ifstream file;
+	ofstream tmp;
+	file.open("new.txt");
+	file_load(file, buffer);
+	tmp.open("~new", ios::out | ios::trunc);
+	
+	while(!exit){
+		flip_screen(buffer);
+		while(detect){
+			key = getch();
+			switch(key){
+				case 0x1B:
+					menu(detect, exit, buffer, tmp, overwrite);
+				break;
+				case 0x0D:
+					buffer.push_back(line_buffer);
+					detect = false;
+				break;
+				case 0x08:
+					line_buffer += "\b \b";
+				break;
+				default:
+					line_buffer += key;
+					cout << key;
+				break;
 			}
-			getline(std::cin, tmp);
-			if(tmp == "eof")
-                break;
-			scr.insert(scr.end(), tmp);
+			flip_screen(buffer);
+			cout << line_buffer;
 		}
-
-		save(scr, file);
-		file.close();
+		detect = true;
+		line_buffer = "";
+		flip_screen(buffer);
+	}
+	
+	file.close();
+	tmp.close();
+	if(overwrite){
+		remove("new.txt");
+		rename("~new", "new.txt");
 	}
 	return 0;
 }
